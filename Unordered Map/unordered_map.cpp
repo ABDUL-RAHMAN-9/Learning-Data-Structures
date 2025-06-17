cceb28c5d5fdc2e60406f75ba7718b3a2ae162ca
Abdul Rahman <abdulrahman161004@gmail.com>
2025-06-17 09:46:47 +0530

    Added implementation of unordered_map operations in C++
    - Initialized an unordered_map with integer keys and string values
    - Inserted elements using both direct assignment and insert()
    - Accessed elements using [] and at()
    - Updated values using element access methods
    - Implemented searching for elements using find()
    - Traversed the unordered_map using both range-based and iterator loops
    - Demonstrated deletion of elements using erase()
    
    This commit provides a structured overview of common operations on unordered_map, showcasing insertion, access, modification, search, iteration, and deletion functionalities.

---
+++ Unordered Map/unordered_map.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+    unordered_map<int, string> um = {
+        {1, "key1"},
+        {2, "key2"},
+        {3, "key3"}};
+
+    cout << endl;
+    // Inserting Elements
+    um[4] = "key4";
+    um.insert({5, "key5"});
+
+    for (auto val : um)
+    {
+        cout << val.first << " : " << val.second << endl;
+    }
+
+    cout << endl;
+    cout << "Accessing Elements" << endl;
+    cout << um[2] << endl;
+    cout << um.at(3) << endl;
+
+    cout << "Updating Elements" << endl;
+    um[2] = "NewKey2";
+    um.at(3) = "NewKey3";
+    cout << endl;
+
+    cout << um[2] << endl;
+    cout << um.at(3) << endl;
+    cout << "Finding Elements" << endl;
+
+    auto it = um.find(2);
+    if (it != um.end())
+    {
+        cout << it->first << ": " << it->second;
+    }
+    else
+    {
+        cout << "Not Found";
+    }
+    cout << endl;
+
+    cout << "Traversing" << endl;
+    for (auto it = um.begin(); it != um.end(); it++)
+    {
+        cout << it->first << " : " << it->second << endl;
+    }
+    cout << endl;
+    cout << "Deleting Elements" << endl;
+    um.erase(2);
+    um.erase(um.begin());
+    for (auto it = um.begin(); it != um.end(); it++)
+    {
+        cout << it->first << " : " << it->second << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
