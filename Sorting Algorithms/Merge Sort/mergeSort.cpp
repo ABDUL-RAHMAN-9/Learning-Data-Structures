80a47e01ed51f4aa113dcf8f98e371afb746f06b
Abdul Rahman <abdulrahman161004@gmail.com>
2025-06-21 23:25:39 +0530

    feat: Add Merge Sort implementation with auxiliary merge helper and display output
    - Implemented mergeSort using divide-and-conquer strategy (O(n log n) time complexity)
    - Defined merge helper function for combining sorted subarrays
    - Included display function to visualize input and output arrays
    - Used vector<int> to manage dynamic array input

--- Sorting Algorithms/Merge Sort/mergeSort.cpp
+++ Sorting Algorithms/Merge Sort/mergeSort.cpp
@@ -2,32 +2,73 @@
 #include <vector>
 using namespace std;
 
-// *
+// ? Time Complexity:- O(n log n)
 
-void display(vector<int> &vec)
+void display(vector<int> &arr)
 {
-    for (auto val : vec)
+    for (auto val : arr)
     {
         cout << val << " ";
     }
     cout << endl;
 }
-
-void mergeSort(vector<int> &vec)
+void merge(vector<int> &arr, int st, int mid, int end)
 {
+    vector<int> temp;
+    int i = st;
+    int j = mid + 1;
+
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else
+        {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    // push remiaining elements
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while (j <= end)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for (int idx = 0; idx < temp.size(); idx++)
+    {
+        arr[idx + st] = temp[idx];
+    }
+}
+
+void mergeSort(vector<int> &arr, int st, int end)
+{
+    if (st < end)
+    {
+        int mid = st + (end - st) / 2;
+        mergeSort(arr, st, mid);      // right half
+        mergeSort(arr, mid + 1, end); // left half
+        merge(arr, st, mid, end);
+    }
 }
 
 int main()
 {
-    vector<int> vec = {12, 11, 13, 5, 6, 4};
+    vector<int> arr = {2, 3, 1, 5, 6, 4};
 
     cout << "Original array: ";
-    display(vec);
+    display(arr);
 
     cout << "Sorted array: ";
-
-    mergeSort(vec);
-    display(vec);
+    mergeSort(arr, 0, arr.size() - 1);
+    display(arr);
 
     return 0;
 }
