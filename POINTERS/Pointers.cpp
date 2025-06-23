7461622c356d52b89e1cad83b6d5eef7a014471e
Abdul Rahman <abdulrahman161004@gmail.com>
2025-06-23 16:25:49 +0530

    Pass by reference using pointer

--- POINTERS/Pointers.cpp
+++ POINTERS/Pointers.cpp
@@ -33,3 +33,20 @@
 //     return 0;
 
 // }
+
+//! Pass by reference using pointer
+#include <iostream>
+using namespace std;
+void changeA(int *ptr)
+{
+    *ptr = 20;
+}
+int main()
+{
+    int a = 10;
+
+    changeA(&a);
+    cout << "value of a is now :" << a;
+
+    return 0;
+}
