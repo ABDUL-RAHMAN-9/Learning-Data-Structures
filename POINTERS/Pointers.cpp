2e9563a1d0a09527e9d6943eac855e1c74288a64
Abdul Rahman <abdulrahman161004@gmail.com>
2025-06-23 16:25:28 +0530

    Created Pointer file ,in which it have pointer code

---
+++ POINTERS/Pointers.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int var1 = 10;
+//     int var2 = 30;
+
+//     int *ptr = &var1;
+//     cout << "Ptr value is now : " << *ptr << endl;
+
+//     // chagning the address
+//     ptr = &var2;
+//     cout << "Change the ptr address to:" << *ptr << endl;
+
+//     // size of ptr
+//     cout << "Size of Pointers: " << sizeof(ptr) << endl;
+//     cout << "\n\n";
+
+//     int a = 10;
+//     int *ptr3 = &a;
+//     int **parentPtr = &ptr3;
+
+//     cout << &a << endl;
+//     cout << ptr3 << endl;
+//     cout << parentPtr << endl;
+
+//     // derefenence the value
+//     cout << *(&a) << endl;
+//     cout << *(ptr3) << endl;
+//     cout << **(parentPtr) << endl;
+
+//     return 0;
+
+// }
