ef8be8cd81e6a711e04ab16ed0f6a24e46d49e5e
Abdul Rahman <abdulrahman161004@gmail.com>
2025-07-07 18:45:21 +0530

    docs: add short definition of Armstrong number in math.cpp

--- MATH/math.cpp
+++ MATH/math.cpp
@@ -1,32 +1,39 @@
-#include <iostream>
-#include <string>
-using namespace std;
+/*
+An Armstrong number is a number that is equal to the
+sum of its digits each raised to the power of the number of digits.
+*/
 
-bool isArmstrong(int n)
-{
-    int copyN = n;
-    int sumOfCubes = 0;
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-    while (n != 0)
-    {
-        int dig = n % 10;
-        sumOfCubes += (dig * dig * dig);
-        n = n / 10;
-    }
-    return sumOfCubes == copyN;
-}
+// bool isArmstrong(int n)
+// {
+//     int copyN = n;
+//     int sumOfCubes = 0;
 
-int main()
-{
-    int n = 153;
-    if (isArmstrong(n))
-    {
-        cout << "Armstrong number";
-    }
-    else
-    {
-        cout << "This is NOT That one!";
-    }
+//     while (n != 0)
+//     {
+//         int dig = n % 10;
+//         sumOfCubes += (dig * dig * dig);
+//         n = n / 10;
+//     }
+//     return sumOfCubes == copyN;
+// }
 
-    return 0;
-}
+// int main()
+// {
+//     int n = 153;
+//     if (isArmstrong(n))
+//     {
+//         cout << "Armstrong number";
+//     }
+//     else
+//     {
+//         cout << "This is NOT That one!";
+//     }
+
+//     return 0;
+// }
+
+// !
