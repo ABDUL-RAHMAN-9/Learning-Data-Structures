37d33102de3441e3951abba961d1e9b2ea53ee61
Abdul Rahman <abdulrahman161004@gmail.com>
2025-07-15 13:03:40 +0530

    feat: implement two-sum solution using hash table for O(n) lookup
    - Added 	woSum function that returns indices of two numbers adding up to a target.
    - Used unordered_map for constant-time lookup of complement values.
    - Fixed syntax error (missing semicolon) in the loop.
    - Corrected index order: returns the index of complement from map and current index.
    - Updated main() to print results with a proper check for no matches.

--- Problems/array.cpp
+++ Problems/array.cpp
@@ -35,45 +35,94 @@
 //     return 0;
 // }
 
-// ! Prefix Sum in array
+// // ! Prefix Sum in array
 
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// using namespace std;
 
-vector<int> findPrefixSum(vector<int> &arr)
-{
-    int n = arr.size();
-    vector<int> prefixSum(n);
+// vector<int> findPrefixSum(vector<int> &arr)
+// {
+//     int n = arr.size();
+//     vector<int> prefixSum(n);
 
-    prefixSum[0] = arr[0];
+//     prefixSum[0] = arr[0];
 
-    for (int i = 1; i < n; i++)
-    {
-        prefixSum[i] = prefixSum[i - 1] + arr[i];
-    }
-    return prefixSum;
-}
+//     for (int i = 1; i < n; i++)
+//     {
+//         prefixSum[i] = prefixSum[i - 1] + arr[i];
+//     }
+//     return prefixSum;
+// }
 
-int main()
-{
-    vector<int> arr = {10, 20, 10, 5, 15};
+// int main()
+// {
+//     vector<int> arr = {10, 20, 10, 5, 15};
 
-    cout << "before: ";
-    for (auto i : arr)
-    {
-        cout << i << " ";
-    }
-    vector<int> prefixSum = findPrefixSum(arr);
-    cout << endl;
-    cout << "After: ";
-    for (auto i : prefixSum)
-    {
-        cout << i << " ";
-    }
+//     cout << "before: ";
+//     for (auto i : arr)
+//     {
+//         cout << i << " ";
+//     }
+//     vector<int> prefixSum = findPrefixSum(arr);
+//     cout << endl;
+//     cout << "After: ";
+//     for (auto i : prefixSum)
+//     {
+//         cout << i << " ";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
+// ! Two sum using hashtable
+// #include <iostream>
+// #include <unordered_map>
+// #include <vector>
+// using namespace std;
 
+// vector<int> twoSum(vector<int> &arr, int target)
+// {
+//     unordered_map<int, int> um;
+//     vector<int> ans;
+
+//     for (int i = 0; i < arr.size(); i++) //  fixed missing semicolon
+//     {
+//         int first = arr[i];
+//         int sec = target - first;
+
+//         if (um.find(sec) != um.end())
+//         {
+//             ans.push_back(um[sec]); //  return first index from map (not current one)
+//             ans.push_back(i);       //  return current index
+//             break;
+//         }
+//         um[first] = i;
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {5, 2, 11, 7, 15};
+//     int target = 9;
+
+//     vector<int> result = twoSum(arr, target);
+
+//     if (!result.empty())
+//     {
+//         cout << "Indices of elements adding up to " << target << ": ";
+//         for (int idx : result)
+//         {
+//             cout << idx << " ";
+//         }
+//         cout << endl;
+//     }
+//     else
+//     {
+//         cout << "No two elements sum up to " << target << endl;
+//     }
+
+//     return 0;
+// }
