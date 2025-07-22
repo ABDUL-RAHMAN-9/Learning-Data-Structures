a38475f7b86df780b42d956fac77f0739c1e4916
Abdul Rahman <abdulrahman161004@gmail.com>
2025-07-22 18:09:58 +0530

    🔁 Added program to generate all permutations using next_permutation()
    - Utilized STL
    ext_permutation to generate lexicographically sorted permutations of a vector
    - Iterated and printed each permutation of the input vector {1, 2, 3}
    - Demonstrates concise usage of C++ STL algorithms for combinatorics

---
+++ Permutations/permutation.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> nums = {1, 2, 3};
+
+//     do
+//     {
+//         for (int n : nums)
+//             cout << n << " ";
+//         cout << endl;
+//     } while (next_permutation(nums.begin(), nums.end()));
+// }
