b88f2a6fba5a3e491426d2472fce48324cdbe1ba
Abdul Rahman <abdulrahman161004@gmail.com>
2025-07-23 12:47:47 +0530

    🔁 Added recursive function to print numbers from N to 1

--- RECURSION/recursion.cpp
+++ RECURSION/recursion.cpp
@@ -30,3 +30,24 @@
 
 //     return 0;
 // }
+
+// ! Printing form n to 1
+// #include <iostream>
+// using namespace std;
+// void printNums(int n)
+// {
+//     if (n < 1)
+//     {
+//         return;
+//     }
+//     cout << n << " ";
+
+//     printNums(n - 1);
+// }
+
+// int main()
+// {
+//     printNums(4);
+
+//     return 0;
+// }
