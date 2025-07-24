7a091d59fb36d1cbfdcbebb008954682a933cd40
Abdul Rahman <abdulrahman161004@gmail.com>
2025-07-24 18:40:43 +0530

    Added recursive check to verify if array is sorted in non-decreasing order

--- RECURSION/recursion.cpp
+++ RECURSION/recursion.cpp
@@ -71,3 +71,27 @@
 
 //     return 0;
 // }
+
+// //! Check if array is shorted or not
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// bool isSorted(vector<int> &arr, int n)
+// {
+//     if (n == 0 || n == 1)
+//     {
+//         return true;
+//     }
+//     return arr[n - 1] >= arr[n - 2] && isSorted(arr, n - 1);
+// }
+// int main()
+// {
+//     vector<int> arr = {1, 2, 3, 4, 5};
+
+//     cout << (isSorted(arr, arr.size()) ? "True" : "False");
+
+//     return 0;
+// }
