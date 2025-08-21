0bc8e89e88d63848299a7ce7969ee94b12b76e9d
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-21 23:53:24 +0530

    - Added function rearrangeArray to rearrange numbers so that positives   appear at even indices and negatives at odd indices - Separated input into pos[] and neg[] vectors before merging alternately - Implemented print helper function to display vectors in [a, b, c] format - Updated main to show both Before rearranging and After rearranging   outputs for better readability - Example:     Before rearranging: [3, 1, -2, -5, 2, -4]     After rearranging : [3, -2, 1, -5, 2, -4]

--- 2 - Pointers/twoPointers.cpp
+++ 2 - Pointers/twoPointers.cpp
@@ -1,43 +1,112 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int removeDuplicates(vector<int> &nums)
+// {
+//     if (nums.empty())
+//         return 0;
+
+//     int i = 0; // slow pointer
+
+//     for (int j = 1; j < nums.size(); j++)
+//     { // fast pointer
+//         if (nums[j] != nums[i])
+//         {
+//             i++;
+//             nums[i] = nums[j];
+//         }
+//     }
+
+//     return i + 1; // New length of unique elements
+// }
+
+// int main()
+// {
+//     vector<int> nums = {1, 2, 3, 3, 2, 4, 5};
+
+//     // Step 1: Sort the array to bring duplicates together
+//     sort(nums.begin(), nums.end());
+
+//     // Step 2: Remove duplicates and get the new size
+//     int newSize = removeDuplicates(nums);
+
+//     // Step 3: Print only the unique part of the array
+//     for (int i = 0; i < newSize; i++)
+//     {
+//         cout << nums[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// ! Rearrange Array Elements by Sign
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
-int removeDuplicates(vector<int> &nums)
+vector<int> rearrangeArray(vector<int> &nums)
 {
-    if (nums.empty())
-        return 0;
+    int n = nums.size();
+    vector<int> pos; // store all positive numbers
+    vector<int> neg; // store all negative numbers
+    vector<int> ans; // final rearranged array
 
-    int i = 0; // slow pointer
-
-    for (int j = 1; j < nums.size(); j++)
-    { // fast pointer
-        if (nums[j] != nums[i])
+    // Separate numbers into positives and negatives
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] > 0)
         {
-            i++;
-            nums[i] = nums[j];
+            pos.push_back(nums[i]);
+        }
+        else
+        {
+            neg.push_back(nums[i]);
         }
     }
 
-    return i + 1; // New length of unique elements
+    int p = 0, q = 0; // counters for pos[] and neg[]
+    // Fill ans alternately: pos at even, neg at odd indices
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            ans.push_back(pos[p++]); // place positive
+        }
+        else
+        {
+            ans.push_back(neg[q++]); // place negative
+        }
+    }
+
+    return ans;
+}
+void print(vector<int> &nums)
+{
+    cout << "[";
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i];
+        if (i != nums.size() - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
 }
 
 int main()
 {
-    vector<int> nums = {1, 2, 3, 3, 2, 4, 5};
+    vector<int> nums = {3, 1, -2, -5, 2, -4};
+    cout << "Before rearranging: ";
+    print(nums);
 
-    // Step 1: Sort the array to bring duplicates together
-    sort(nums.begin(), nums.end());
+    vector<int> ans = rearrangeArray(nums);
 
-    // Step 2: Remove duplicates and get the new size
-    int newSize = removeDuplicates(nums);
-
-    // Step 3: Print only the unique part of the array
-    for (int i = 0; i < newSize; i++)
-    {
-        cout << nums[i] << " ";
-    }
+    cout << "After rearranging : ";
+    print(ans);
 
     return 0;
 }
