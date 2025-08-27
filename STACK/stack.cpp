7f8353b5d56ff1d1d2c6d52c4b1f1eb15c298d5a
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-27 18:39:55 +0530

    Created Stack Folder for stack problem set practice

---
+++ STACK/stack.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+    s.push(4);
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    cout << s.top() << endl;
+    cout << s.size() << endl;
+    
+
+    while (!s.empty())
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    return 0;
+}
