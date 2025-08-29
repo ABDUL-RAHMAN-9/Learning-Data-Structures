a2d2c608962b5a1a57db7521f665da7e50720c1d
Abdul Rahman <abdulrahman161004@gmail.com>
2025-08-29 16:39:40 +0530

    Implement custom Stack class using vector in C++ with push, pop, top, and empty methods; added main function to demonstrate stack operations by pushing values (10, 20, 30) and displaying them in LIFO order.

--- STACK/stack.cpp
+++ STACK/stack.cpp
@@ -1,24 +1,71 @@
-#include <iostream>
-#include <stack>
-using namespace std;
 
-int main()
-{
-    stack<int> s;
-    s.push(4);
-    s.push(3);
-    s.push(2);
-    s.push(1);
 
-    cout << s.top() << endl;
-    cout << s.size() << endl;
-    
+// #include <iostream>
+// #include <stack>
+// using namespace std;
 
-    while (!s.empty())
-    {
-        cout << s.top() << endl;
-        s.pop();
-    }
+// int main()
+// {
+//     stack<int> s;
+//     s.push(4);
+//     s.push(3);
+//     s.push(2);
+//     s.push(1);
 
-    return 0;
-}
+//     cout << s.top() << endl;
+//     cout << s.size() << endl;
+
+//     while (!s.empty())
+//     {
+//         cout << s.top() << endl;
+//         s.pop();
+//     }
+
+//     return 0;
+// }
+
+// ! Implementing stack using vector
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// class Stack
+// {
+//     vector<int> vec;
+
+// public:
+//     void push(int val)
+//     {
+//         vec.push_back(val);
+//     }
+
+//     void pop()
+//     {
+//         vec.pop_back();
+//     }
+//     int top()
+//     {
+//         return vec[vec.size() - 1];
+//     }
+
+//     bool empty()
+//     {
+//         return vec.size() == 0;
+//     }
+// };
+// int main()
+// {
+//     Stack s;
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
+
+//     cout << "Displaying Stack values: " << endl;
+
+//     while (!s.empty())
+//     {
+//         cout << s.top() << " ";
+//         s.pop();
+//     }
+
+//     return 0;
+// }
