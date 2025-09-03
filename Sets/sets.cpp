c6a106099741bb10ad3eb75237618aab07aa706a
Abdul Rahman <abdulrahman161004@gmail.com>
2025-09-03 12:20:48 +0530

    Added C++ example demonstrating set (ordered set) usage
    - Implemented a set<int> to store unique integers in sorted order.
    - Inserted elements (10, 20, 30) and showed duplicate insertion (20) is ignored.
    - Displayed elements in ascending order automatically maintained by set.
    - Checked existence of element 20 using find().
    - Demonstrated erase() by removing element 10.
    - Printed updated contents of the set and its size.
    - Highlighted difference from unordered_set: maintains sorted order.

--- Sets/sets.cpp
+++ Sets/sets.cpp
@@ -1,5 +1,5 @@
 // ! 02-sept-2025
-// * Set :- A Set is a collection of unique elements (no duplicates).
+// * Set :- When you need uniqueness + fastest lookups, order doesn’t matter.
 
 // #include <iostream>
 // #include <unordered_set>
@@ -46,6 +46,55 @@
 
 //     //  size of the set
 //     cout << "Set size : " << mySet.size() << endl;
+
+//     return 0;
+// }
+
+// ! set Use: - When you need uniqueness + sorted order.
+
+// #include <iostream>
+// #include <set>
+// using namespace std;
+
+// int main()
+// {
+//     // Create a set of integers
+//     set<int> mySet;
+
+//     // Insert elements
+//     mySet.insert(10);
+//     mySet.insert(40);
+//     mySet.insert(20);
+//     mySet.insert(30);
+//     mySet.insert(20); // duplicate, won't be added
+
+//     // Display elements (sorted order)
+//     cout << "Elements in set: ";
+//     for (int num : mySet)
+//     {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+
+//     // Check if an element exists
+//     if (mySet.find(20) != mySet.end())
+//     {
+//         cout << "20 is in the set" << endl;
+//     }
+
+//     // Erase an element
+//     mySet.erase(10);
+
+//     // Display after erase
+//     cout << "After erasing 10: ";
+//     for (int num : mySet)
+//     {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+
+//     // Size of set
+//     cout << "Set size: " << mySet.size() << endl;
 
 //     return 0;
 // }
