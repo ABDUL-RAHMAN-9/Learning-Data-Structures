7ff63fad0d034a7993a8f7ba67a2ba1af2f96d75
Abdul Rahman <abdulrahman161004@gmail.com>
2025-09-03 11:59:30 +0530

    Implement stack using linked list in C++
    - Created a Stack class using STL list<int> as underlying container.
    - Added push, pop, top, and empty methods for stack functionality.
    - Implemented main function to demonstrate stack operations by pushing 10, 20, 30 and printing elements in LIFO order.

--- STACK/stack.cpp
+++ STACK/stack.cpp
@@ -69,3 +69,49 @@
 
 //     return 0;
 // }
+
+// ! Stack using Linked List
+// #include <iostream>
+// #include <list>
+// using namespace std;
+
+// class Stack
+// {
+//     list<int> ll;
+
+// public:
+//     void push(int val)
+//     {
+//         ll.push_front(val);
+//     }
+
+//     void pop()
+//     {
+//         ll.pop_front();
+//     }
+
+//     int top()
+//     {
+//         return ll.front();
+//     }
+//     bool empty()
+//     {
+//         return ll.size() == 0;
+//     }
+// };
+
+// int main()
+// {
+//     Stack s;
+//     s.push(10);
+//     s.push(20);
+//     s.push(30);
+//     cout << "Implementing stack using Linked List: " << endl;
+//     while (!s.empty())
+//     {
+//         cout << s.top() << " ";
+//         s.pop();
+//     }
+
+//     return 0;
+// }
